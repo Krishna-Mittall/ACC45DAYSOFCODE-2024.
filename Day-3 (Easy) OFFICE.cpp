e551ddeca6352359518588a406bcf5c94a7d263d
Krishna-Mittall <krishnamittal9691@gmail.com>
2024-09-30 18:31:29 +0530

    Add files via upload

---
+++ Day-3 (Easy) OFFICE.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int T;
+	cin >> T;
+	int p;
+	for(int i=1; i<=T; i++){
+	    int X,Y;
+	    cin >> X >> Y;
+	    
+	    p = (4*X) + Y;
+	    cout<<p<<endl;
+	}
+
+}
