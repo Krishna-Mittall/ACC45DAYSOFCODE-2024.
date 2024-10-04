8d01ee3f5d06dd81993377229f6943f9e682a6c1
Krishna-Mittall <krishnamittal9691@gmail.com>
2024-10-04 22:59:37 +0530

    Add files via upload

---
+++ Day7- (Easy) JASSIGNMENTS.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int T;
+	cin >> T;
+	
+	while(T--){
+	    int x;
+	    cin >> x;
+	    
+	    if(x+3<=10){
+	        cout<<"YES"<<endl;
+	    }
+	    else
+	        cout<<"NO"<<endl;
+	}
+
+}
